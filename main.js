d01b504ad7f54e852edfb66cae164506fe28c47e
Irene Fabbri <irenefabbri.ed@gmail.com>
2025-02-07 15:32:59 +0100

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello, world')
+const { argv } = require('node:process');
+const { crawlPage } = require('./crawl.js')
+
+function main(){
+    const CLIs = argv.length - 2
+    if (CLIs < 1){
+        console.log("no website provided")
+        process.exit(1)
+    } else if (CLIs > 1) {
+        console.log("too many command line arguments")
+        process.exit(1)
+    } 
+    const baseURL = argv[2]
+    console.log(`Starting crowl of ${baseURL}`)
+    crawlPage(baseURL)
+}
+  
+main()
